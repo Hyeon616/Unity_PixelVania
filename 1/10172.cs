2573859bb13fb2ccf2ac6df29b7a610c995f3284
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2023-09-12 19:14:09 +0900

    Create 10172.cs

---
+++ 1/10172.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10172. 아래 예제와 같이 개를 출력하시오.
+            // \ 랑 " 를 출력하려면 \를 붙여야함.
+          
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )\"\"\"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
+
+        }
+    }
+}
