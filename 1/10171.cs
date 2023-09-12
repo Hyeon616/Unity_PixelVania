7bb3b9569f607ea6b5828fb4f2778678615705ad
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2023-09-12 19:16:26 +0900

    Create 10171.cs

---
+++ 1/10171.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAEKJOON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10171. 아래 예제와 같이 고양이를 출력하시오.
+            // \를 출력하려면 앞에 \를 붙여야함.
+          
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+
+        }
+    }
+}
