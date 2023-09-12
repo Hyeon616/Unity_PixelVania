9e23059d2b131def3f03e70f14705cc030381856
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2023-09-12 17:31:21 +0900

    Update and rename AxB.cs to A×B.cs

--- 1/A×B.cs
+++ 1/A×B.cs
@@ -11,6 +11,7 @@ namespace BAEKJOON
     {
         static void Main(string[] args)
         {
+            
             string [] input = Console.ReadLine().Split();
             Console.WriteLine(int.Parse(input[0]) * int.Parse(input[1]));
         }
