c816f8eb6993475779724115b782c63218a4fe75
Hyeon616 <94216776+Hyeon616@users.noreply.github.com>
2023-09-18 10:48:16 +0900

    Update 2739.cs

--- 3/2739.cs
+++ 3/2739.cs
@@ -6,6 +6,7 @@ namespace BAEKJOON
     {
         static void Main(string[] args)
         {
+            // 2739. N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
            int A = int.Parse(Console.ReadLine());
 
            for (int i = 1; i < 10; i++)
